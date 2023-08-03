cc753ba715c06f572e5aeb7dea3bc0f6871c3308
Serg Blitzen <sergblitzen@gmail.com>
2023-08-03 20:10:37 +0300

    fix: clear code and readme

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -19,22 +19,29 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
     'colorfield',
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
     'djoser',
+]
+
+LOCAL_APPS = [
     'recipes',
     'users'
 ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
